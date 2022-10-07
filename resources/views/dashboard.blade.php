a1a335c134c78da108ac07fcf86e68f9a7239889
laijinfatt <jinfatt124192@gmail.com>
2022-10-07 10:17:45 +0800

    agent login success

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,11 @@
-@extends('auth.layout')
+@extends('layout')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                @if(auth()->user()->isAdmin())
+                <div class="card-header">{{ __('Admin Dashboard') }}</div>
   
                 <div class="card-body">
                     @if (session('success'))
@@ -18,6 +19,22 @@
                     <a class="nav-link" href="{{ route('agent.register') }}">Register an agent here!</a>
                 
                 </div>
+                @elseif(auth()->user()->isAgent())
+                <div class="card-header">{{ __('Agent Dashboard') }}</div>
+  
+                <div class="card-body">
+                    @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+  
+                    You are Logged In as an agent. 
+                    
+                    <a class="nav-link" href="#">Register new user here!</a>
+                
+                </div>
+                @endif
             </div>
         </div>
     </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -59,9 +59,6 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">Login</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">Register</a>
-                            </li>
                         @else
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('logout') }}">Logout</a>
