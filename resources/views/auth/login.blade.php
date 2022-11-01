e0dbb75faed416983437ff7a0b30410ceb64d841
laijinfatt <jinfatt124192@gmail.com>
2022-11-01 11:02:48 +0800

    toggle password show

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,8 +13,23 @@
         <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
         <script src="jquery.3.4.1.js"></script>
         <script src="all.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link 
+  href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" 
+  rel="stylesheet"  type='text/css'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
 
-<main class="login-form">
+<script type="text/javascript">
+$(document).on('click', '.toggle-password', function() {
+
+$(this).toggleClass(" fa-eye fa-eye-slash");
+
+var input = $("#password");
+input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
+});
+</script>
+
+ <main class="login-form">
   <div class="cotainer">
       <div class="row justify-content-center">
           <div class="col-md-8">
@@ -25,7 +40,7 @@
                 <span class="crose" data-dismiss="alert">&times;</span>
             </div></center>
         @endif
-              <div class="card">
+              <div class="card" style="margin-top:15px;">
                   <div class="card-header">Login</div>
                   <div class="card-body">
   
@@ -53,15 +68,20 @@
   
                           <div class="form-group row">
                               <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-                              <div class="col-md-6">
+                              <div class="col-md-6" style='white-space: nowrap'>
+                              <i toggle="#password-field" class="fa-fw fa fa-eye-slash field_icon toggle-password" 
+                              style="position:absolute;margin-left:24rem;margin-top:0.7rem;color:grey;"></i>
                               <input type="password" id="password" class="form-control" name="password" placeholder="Password" required @if(Cookie::has('password')) value="{{Cookie::get('password')}}" @endif>
-                                  @if ($errors->has('password'))
+                           
+                            
+                              @if ($errors->has('password'))
                                       <span class="text-danger">{{ $errors->first('password') }}</span>
                                   @endif
                               </div>
                           </div>
+
   
-                          <div class="form-group row">
+                          <div class="form-group row"  style="margin-bottom:10px !important;">
                               <div class="col-md-6 offset-md-4">
                                   <div class="checkbox">
                                       <label>
@@ -75,7 +95,7 @@
                               <div class="col-md-6 offset-md-4">
                                   <div class="checkbox">
                                       <label>
-                                          <a href="{{ route('forget.password.get') }}">Reset Password</a>
+                                          <a href="{{ route('forget.password.get') }}" style="text-decoration:none;">Forget Password?</a>
                                       </label>
                                   </div>
                               </div>
@@ -87,6 +107,7 @@
                               </button>
                           </div>
                       </form>
+ 
                         
                   </div>
               </div>
@@ -94,4 +115,5 @@
       </div>
   </div>
 </main>
+
 @endsection
